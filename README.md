b516a9b973cddc42e03051f368febfc6b77c1d53
zmichgen <zmichgen@gmail.com>
2019-02-07 22:51:26 +0300

    update

---
+++ README.md
@@ -0,0 +1,3 @@
+# dashboard
+
+[DEMO](https://zmichgen.github.io/dashboard)
---
+++ precache-manifest.13700431416a3fedfef84aa47af1b35b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dashboard/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e56aae448bf7b52adfbc1ec79aa599e3",
+    "url": "/dashboard/static/media/RSSBands.e56aae44.svg"
+  },
+  {
+    "revision": "81312d4a81e499095a3d",
+    "url": "/dashboard/static/js/runtime~main.81312d4a.js"
+  },
+  {
+    "revision": "0ee01b2eef44d4f6e9a0",
+    "url": "/dashboard/static/js/main.0ee01b2e.chunk.js"
+  },
+  {
+    "revision": "8aa35610b89d34ee947b",
+    "url": "/dashboard/static/js/1.8aa35610.chunk.js"
+  },
+  {
+    "revision": "0ee01b2eef44d4f6e9a0",
+    "url": "/dashboard/static/css/main.226effd8.chunk.css"
+  },
+  {
+    "revision": "8aa35610b89d34ee947b",
+    "url": "/dashboard/static/css/1.f4458d78.chunk.css"
+  },
+  {
+    "revision": "d7a431c85d45918802008bb9ff3285ea",
+    "url": "/dashboard/index.html"
+  }
+];
